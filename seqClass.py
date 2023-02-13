f6d0096ee08bbbf300e3ab514e106d9a6a0b5438
marinavilardell <marina.vilardell@uvic.cat>
2023-02-13 18:04:03 +0100

    code commented

--- seqClass.py
+++ seqClass.py
@@ -14,21 +14,21 @@ if len(sys.argv) == 1:
 args = parser.parse_args()
 
 args.seq = args.seq.upper()
-if re.search('^[ACGTU]+$', args.seq):
-    if re.search('T', args.seq):
+if re.search('^[ACGTU]+$', args.seq): 
+    if re.search('T', args.seq):  #If we find a T, for sure it will be DNA
         print ('The sequence is DNA')
-    elif re.search('U', args.seq):
+    elif re.search('U', args.seq): #If we find a U, the sequence will be RNA
         print ('The sequence is RNA')
-    else:
+    else: #If we have a sequence that contains A,C, and G, we don't know if it is RNA or DNA.
         print ('The sequence can be DNA or RNA')
-else:
+else: #If the input sequence contains a letter that is not A,C,G,T or U, it is not DNA nor RNA
     print ('The sequence is not DNA nor RNA')
 
 if args.motif:
     args.motif = args.motif.upper()
     print(f'Motif search enabled: looking for motif "{args.motif}" in sequence "{args.seq}"... ', end = '')
-    if re.search(args.motif, args.seq):
+    if re.search(args.motif, args.seq): #if the given motif is within the sequence, it is found
         print("FOUND")
-    else:
+    else: #if not, the motif  is not found in the sequence
         print("NOT FOUND MOTIF")
 
