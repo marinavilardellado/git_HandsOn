cc217a523e35a810b10ee08b119eb0b9f6318e31
marinavilardell <marina.vilardell@uvic.cat>
2023-02-14 10:00:36 +0100

    changes fixed

--- seqClass.py
+++ seqClass.py
@@ -14,17 +14,23 @@ if len(sys.argv) == 1:
 args = parser.parse_args()
 
 args.seq = args.seq.upper()
-if re.search('^[ACGTU]+$', args.seq): 
-    if re.search('T', args.seq):  #If we find a T, for sure it will be DNA
-        print ('The sequence is DNA')
-    elif re.search('U', args.seq): #If we find a U, the sequence will be RNA
-        print ('The sequence is RNA')
-    else: #If we have a sequence that contains A,C, and G, we don't know if it is RNA or DNA.
-        print ('The sequence can be DNA or RNA')
-else: #If the input sequence contains a letter that is not A,C,G,T or U, it is not DNA nor RNA
-    print ('The sequence is not DNA nor RNA')
 
+# Check if the sequence is DNA or RNA
+if re.search("^[ACGTU]+$", args.seq):
+    if re.search("^[^U]+$", args.seq) and re.search("T", args.seq):
+        print("The sequence is DNA")
+    elif re.search("^[^T]+$", args.seq) and re.search("U", args.seq):
+        print("The sequence is RNA")
+    elif re.search("^[^T]+$", args.seq) and re.search("^[^U]+$", args.seq):
+        print("The sequence can be DNA or RNA")
+    else:
+        print("Your sequence is mixted")
+else:
+    print("The sequence is not DNA neither RNA")
+
+#Motifs
 if args.motif:
+    #convert it to upper cases
     args.motif = args.motif.upper()
     print(f'Motif search enabled: looking for motif "{args.motif}" in sequence "{args.seq}"... ', end = '')
     if re.search(args.motif, args.seq): #if the given motif is within the sequence, it is found
